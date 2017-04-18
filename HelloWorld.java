8b05b6e58f54b0b60c3476f472ef2944ab3fe68c
Matthew Bowers <Matthew.Allen.Bowers@gmail.com>
2017-04-18 14:06:39 -0400

    fixed class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@
  * HelloWorld.java
  */
 
-public static void main(String[] args){ 
-	System.out.println("Hello World!");
+public class HelloWorld{
+
+	public static void main(String[] args){ 
+		System.out.println("Hello World!");
+	}
 }
